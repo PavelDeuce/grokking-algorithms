eb0029cc1dc14eb267990d06591d001e2b6389a9
paveldeuce <deucela.pavel@gmail.com>
2021-09-12 22:03:19 +0300

    add binarySearch

--- __tests__/chapter-1.test.js
+++ __tests__/chapter-1.test.js
@@ -1,3 +1,12 @@
-test('example-test', () => {
-  expect(1).toBe(1);
+import binarySearch from '../src/chapters/chapter-1/binarySearch.js';
+
+test('Binary search', () => {
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 14, 15];
+
+  expect(binarySearch(array, 20)).toBe(-1);
+  expect(binarySearch([], 5)).toBe(-1);
+
+  expect(binarySearch(array, 8)).toBe(7);
+  expect(binarySearch(array, 4)).toBe(3);
+  expect(binarySearch(array, 15)).toBe(14);
 });
---
+++ src/chapters/chapter-1/binarySearch.js
@@ -0,0 +1,23 @@
+const binarySearch = (list, value) => {
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const guess = list[mid];
+
+    if (guess < value) {
+      low = mid + 1;
+      continue;
+    }
+    if (guess > value) {
+      high = mid - 1;
+      continue;
+    }
+    return mid;
+  }
+
+  return -1;
+};
+
+export default binarySearch;
